a6fc5a13b3b5c6e3aa149e31489ef9f6d276a2e6
Aman Pratik <amanpratik10@gmail.com>
2015-09-13 15:57:16 +0530

    major style changes

--- blogu/forms.py
+++ blogu/forms.py
@@ -5,8 +5,8 @@ class BlogForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the blog")
     views = forms.IntegerField(widget=forms.HiddenInput,initial=0)
     slug = forms.CharField(widget=forms.HiddenInput, required=False)
-    image=forms.ImageField(required=False)
-    image_description=forms.CharField(widget=forms.Textarea,required=False)
+    image=forms.ImageField(required=False,help_text="Upload image")
+    image_description=forms.CharField(widget=forms.Textarea,required=False,help_text="Say something about the image")
     text = forms.CharField(widget=forms.Textarea,help_text="Enter the blog here")
     likes=forms.IntegerField(widget=forms.HiddenInput,initial=0)
     
